be4741fdc2e841366a22fd0a9adc988f5c87c682
Sergio Quintero <sergio.quinteroc@udea.edu.co>
2022-11-21 13:28:56 -0500

    Renderizar mapas

--- game.js
+++ game.js
@@ -2,12 +2,12 @@ const canvas = document.querySelector("#game");
 const game = canvas.getContext("2d");
 
 window.addEventListener("load", setCanvasSize);
-window.addEventListener('resize', setCanvasSize);
+window.addEventListener("resize", setCanvasSize);
 
 let canvasSize;
 let elementSize;
 
-function setCanvasSize(){
+function setCanvasSize() {
     const wHeight = window.innerHeight;
     const wWidth = window.innerWidth;
     canvasSize = wHeight < wWidth ? wHeight * 0.8 : wWidth * 0.8;
@@ -15,13 +15,12 @@ function setCanvasSize(){
     canvas.setAttribute("width", canvasSize);
     canvas.setAttribute("height", canvasSize);
 
-     elementSize = (canvasSize / 10);
+    elementSize = canvasSize / 10;
     // elementSize = (canvasSize / 10) - 1;
     startGame();
 }
 
 function startGame() {
-
     console.log({
         canvasSize,
         elementSize,
@@ -30,10 +29,17 @@ function startGame() {
     game.font = elementSize + "px Verdana";
     game.textAlign = "end";
     //game.textAlign = "";
-    
-    for (let i = 1; i <= 10; i++) {
-        game.fillText(emojis["X"], elementSize * i, elementSize);
-    }
 
+    const map = maps[0];
+    const mapRow = map.trim().split("\n");
+    const mapRowCol = mapRow.map((row) => row.trim().split(""));
+
+    mapRowCol.forEach((row, x) => {
+        row.forEach((col, y) => {
+            const emoji = emojis[col];
+            const posX = elementSize * (y + 1) + 5;
+            const posY = elementSize * (x + 1) - 5;
+            game.fillText(emoji, posX, posY);
+        });
+    });
 }
-
